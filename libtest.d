8cf3f29e1c7b475f987196bb603cc5ed29fbc5e7
dasdias <das.dias6@gmail.com>
2022-03-04 01:02:46 +0000

    Repo structure changes + .gitignore changes

--- libtest.d
+++
@@ -1,4 +0,0 @@
-obj/libtest.o: src/libtest.c src/clic.h src/cliccommand.h \
-  src/lib/uthash/src/uthash.h src/clicshell.h \
-  src/lib/c-consolelogger/src/consolelogger.h \
-  src/lib/c-consolelogger/src/lib/c-sformat/src/sformat.h
